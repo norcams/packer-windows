65853881e5655053b4b9ddffe686b22bb608fe70
edpto <tor.ladre@it.uib.no>
2024-12-13 16:47:58 +0100

    Actually set build var

--- packer_templates/scripts/windows/disable-recovery.ps1
+++ packer_templates/scripts/windows/disable-recovery.ps1
@@ -1,6 +1,7 @@
 # By default Server 2025 and later insist on creating a recovery partition after the system partition
 # instead of accepting the system32/recovery location. We need to delete this partition in order to
 # allow the system drive to expand when deploying to OpenStack.
+$winbuild=$([System.Environment]::OSVersion.Version.Build)
 if ($winbuild -ge 26100) {
   Write-Host 'Delete the recovery partition'
   reagentc /disable
